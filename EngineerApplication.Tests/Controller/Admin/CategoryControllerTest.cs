3129b8cb5cdbf26ac1a9c4e4a4ff47d3460fa3d8
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2022-03-07 01:25:59 +0100

    feat: add test structure of project

---
+++ EngineerApplication.Tests/Controller/Admin/CategoryControllerTest.cs
@@ -0,0 +1,84 @@
+﻿using EngineerApplication.Areas.Admin.Controllers;
+using EngineerApplication.ContextStructure.Data.Service.Interfaces;
+using EngineerApplication.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace EngineerApplication.Tests.Controller.Admin
+{
+  public class CategoryControllerTest : BaseControllerTest
+  {
+    private Mock<IUnitOfWork> _unitOfWork;
+    private CategoryController _categoryController;
+
+    [SetUp]
+    public void Setup()
+    {
+      _unitOfWork = new Mock<IUnitOfWork>();
+      _categoryController = new CategoryController(_unitOfWork.Object);
+    }
+    [Test]
+    public async Task TestGetMethodForHomePage()
+    {
+      var response = await Client.GetAsync("/");
+      response.EnsureSuccessStatusCode();
+      var responseString = await response.Content.ReadAsStringAsync();
+      var result = responseString != null && responseString.Contains("Bluzy Sportowe");
+      Assert.That(result);
+    }
+
+    [Test]
+    public async Task TestPostCategoryMethodForPage()
+    {
+      var categoryItem = new Category()
+      {
+        Name = "Szczotki Sportowe",
+        DisplayOrder = 8
+      };
+      var category = await Client.PostAsJsonAsync("/Admin/Controller", categoryItem);
+      Assert.IsNotNull(category.RequestMessage.Content);
+    }
+
+    [Test]
+    public async Task TestPutCategoryMethodForPage()
+    {
+      var categoryItem = new Category()
+      {
+        Name = "Szczotki Sportowe",
+        DisplayOrder = 9
+      };
+      var category = await Client.PostAsJsonAsync("/Admin/Controller/1", categoryItem);
+      Assert.IsNotNull(category.RequestMessage);
+    }
+
+    [Test]
+    public async Task TestDeleteCategoryMethodForPage()
+    {
+      var category = await Client.DeleteAsync("/Admin/Controller/1");
+      Assert.IsNotNull(category.RequestMessage);
+    }
+
+    [Test]
+    public async Task TestGetByIdCategoryMethodForPage()
+    {
+      var category = await Client.GetAsync("/Admin/Controller/1");
+      var result = category.RequestMessage;
+      Assert.IsNotNull(result);
+    }
+
+    [Test]
+    public async Task TestGetAllCategoryMethodForPage()
+    {
+      var category = await Client.GetAsync("/Admin/Controller");
+      var result = category.RequestMessage;
+      Assert.IsNotNull(result);
+    }
+  }
+}
---
+++ EngineerApplication.Tests/Controller/BaseControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace EngineerApplication.Tests.Controller
+{
+  public abstract class BaseControllerTest
+  {
+    private readonly IHostBuilder _hostBuilder;
+    protected IHost Application;
+    public HttpClient Client;
+
+    protected BaseControllerTest()
+    {
+      _hostBuilder = Host
+        .CreateDefaultBuilder()
+        .ConfigureAppConfiguration(config =>
+        {
+          config.AddInMemoryCollection(new Dictionary<string, string>() { ["ConnectionStrings:Database"] = @"Data Source=DESKTOP-VPKE3ES\\SQLEXPRESS;Initial Catalog=EngineerDatabase;Integrated Security=True" });
+          config.AddEnvironmentVariables();
+        })
+        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+        .ConfigureWebHost(webHost => { webHost.UseIISIntegration(); });
+    }
+
+
+    [OneTimeSetUp]
+    public void StartApplication()
+    {
+      var webApplicationFactory = new WebApplicationFactory<Program>();
+      TestServer testServer = webApplicationFactory.Server;
+      Client = webApplicationFactory.WithWebHostBuilder(builder
+          => builder.ConfigureTestServices(services =>
+          {
+            // allow anonymous access to bypass authorization
+            services.AddMvc(opt => opt.Filters.Add(new AllowAnonymousFilter()));
+          })).CreateClient();
+    }
+
+    [OneTimeTearDown]
+    public void StopApplication()
+    {
+      Application.Dispose();
+    }
+  }
+}
+
