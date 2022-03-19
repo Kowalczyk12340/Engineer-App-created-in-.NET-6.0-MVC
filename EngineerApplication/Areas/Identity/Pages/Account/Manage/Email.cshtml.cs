6ba51f46bcecca95f3b2c428fb039e8bf7b2c500
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2022-03-19 13:38:40 +0100

    feat: add resources to the application and translating for english, german and polish

--- EngineerApplication/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ EngineerApplication/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
