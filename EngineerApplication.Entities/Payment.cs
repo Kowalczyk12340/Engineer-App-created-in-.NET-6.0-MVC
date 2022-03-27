7d755271023a67252a6b7bdd18d4512f48cb9398
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2022-03-28 01:28:51 +0200

    feat: add payment entity to the project without migration

---
+++ EngineerApplication.Entities/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineerApplication.Entities
+{
+  public class Payment
+  {
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [Display(Name = "Payment Name")]
+    public string? Name { get; set; }
+
+    [Required]
+    [Display(Name = "Payment Code")]
+    public string? Code { get; set; }
+  }
+}
