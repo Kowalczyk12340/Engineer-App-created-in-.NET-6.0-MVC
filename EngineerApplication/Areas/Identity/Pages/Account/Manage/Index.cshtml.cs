d5ea5963e5887525d7303f13577e792f9adde582
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2022-04-13 16:37:08 +0200

    feat: improve the managing profile and improving summary of displaying cart

--- EngineerApplication/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ EngineerApplication/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -32,9 +32,11 @@ namespace EngineerApplication.Areas.Identity.Pages.Account.Manage
 
     public class InputModel
     {
+      [Display(Name = "Email")]
+      public string? Email { get; set; }
       [Phone]
       [Display(Name = "Phone number")]
-      public string PhoneNumber { get; set; }
+      public string? PhoneNumber { get; set; }
     }
 
     public async Task<IActionResult> OnGetAsync()
@@ -45,12 +47,14 @@ namespace EngineerApplication.Areas.Identity.Pages.Account.Manage
         return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
       }
       var userName = await _userManager.GetUserNameAsync(user);
+      var email = await _userManager.GetEmailAsync(user);
       var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 
       Username = userName;
 
       Input = new InputModel
       {
+        Email = email,
         PhoneNumber = phoneNumber
       };
 
@@ -68,6 +72,17 @@ namespace EngineerApplication.Areas.Identity.Pages.Account.Manage
       if (user == null)
       {
         return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+      }
+
+      var email = await _userManager.GetEmailAsync(user);
+      if (Input.Email != email)
+      {
+        var setEmailResult = await _userManager.SetEmailAsync(user, Input.Email);
+        if (!setEmailResult.Succeeded)
+        {
+          var userId = await _userManager.GetUserIdAsync(user);
+          throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
+        }
       }
 
       var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
