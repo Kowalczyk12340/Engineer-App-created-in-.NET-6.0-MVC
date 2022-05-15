7a3d8adf237be14b0fcf5a6968c5983a792d6e4b
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2022-05-15 22:18:25 +0200

    fix: delete unuseful data and add the script of datas with migrations

--- EngineerApplication.Tests/ServiceTest/CommodityServiceTest.cs
+++ EngineerApplication.Tests/ServiceTest/CommodityServiceTest.cs
@@ -4,7 +4,6 @@ using EngineerApplication.Entities;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
-using System.Threading.Tasks;
 
 namespace EngineerApplication.Tests.ServiceTest
 {
