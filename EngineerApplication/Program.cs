e215e7a3ea5b77c444a7cc3c7e1a79db69bc1576
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2022-05-16 11:22:47 +0200

    feat: improve the version of start application by deleting startup class and adjust a program class regarding to the requirements in .net 6.0

--- EngineerApplication/Program.cs
+++ EngineerApplication/Program.cs
@@ -1,17 +1,140 @@
-namespace EngineerApplication
-{
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      CreateHostBuilder(args).Build().Run();
-    }
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using EngineerApplication.ContextStructure.Data;
+using EngineerApplication.ContextStructure.Data.Initializer;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using EngineerApplication.Helpers;
+using EngineerApplication.ContextStructure.Data.Service.Interfaces;
+using EngineerApplication.ContextStructure.Data.Service;
+using EngineerApplication.Resources;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
+var builder = WebApplication.CreateBuilder(args);
+
+var config = builder.Configuration;
+var services = builder.Services;
+
+#if DEBUG
+services.AddMvc(options => options.EnableEndpointRouting = false).AddRazorRuntimeCompilation();
+#else
+    services.AddMvc(options => options.EnableEndpointRouting = false);
+#endif
+
+services.Configure<CookiePolicyOptions>(options =>
+{
+  options.CheckConsentNeeded = context => true;
+});
+
+services.AddDbContext<EngineerDbContext>(options =>
+    options.UseSqlServer(
+        config.GetConnectionString("Database")))
+  .AddDbContext<EngineerDbContext>(options =>
+    options.UseSqlServer(
+        config.GetConnectionString("AzureDatabase")));
+services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<EngineerDbContext>()
+    .AddDefaultTokenProviders();
+
+services.Configure<RequestLocalizationOptions>(options =>
+{
+  var supportedCultures = new[]
+  {
+          new CultureInfo("pl"),
+          new CultureInfo("en"),
+          new CultureInfo("de")
+        };
+  options.DefaultRequestCulture = new RequestCulture("en");
+  options.SupportedCultures = supportedCultures;
+  options.SupportedUICultures = supportedCultures;
+});
+
+services.AddSingleton<IEmailSender, EmailSender>();
+services.AddMvc().AddViewLocalization().AddRazorOptions(options =>
+{
+  options.ViewLocationFormats.Add("/{0}.cshtml");
+});
+services.AddSingleton<CommonLocalizationService>();
+services.AddScoped<IUnitOfWork, UnitOfWork>();
+services.AddScoped<ISeederToDatabase, SeederToDatabase>();
+services.AddSession(options =>
+{
+  options.IdleTimeout = TimeSpan.FromMinutes(30);
+  options.Cookie.HttpOnly = true;
+  options.Cookie.IsEssential = true;
+});
+
+services.Configure<IdentityOptions>(options =>
+{
+  options.Password.RequireDigit = true;
+  options.Password.RequiredLength = 8;
+  options.Password.RequireNonAlphanumeric = false;
+  options.Password.RequireUppercase = true;
+  options.Password.RequireLowercase = false;
+  options.Password.RequiredUniqueChars = 6;
+
+  options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+  options.Lockout.MaxFailedAccessAttempts = 10;
+  options.Lockout.AllowedForNewUsers = true;
+
+  options.User.RequireUniqueEmail = true;
+});
+
+services.ConfigureApplicationCookie(options =>
+{
+  options.Cookie.HttpOnly = true;
+  options.ExpireTimeSpan = TimeSpan.FromDays(300);
+  options.LoginPath = "/Account/Login";
+  options.AccessDeniedPath = "/Account/AccessDenied";
+  options.SlidingExpiration = true;
+});
+
+services.AddLocalization(options => options.ResourcesPath = "Resources");
+services.AddControllersWithViews().AddNewtonsoftJson().AddRazorRuntimeCompilation();
+services.AddRazorPages();
+
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+  app.UseDeveloperExceptionPage();
+  app.UseMigrationsEndPoint();
+}
+else
+{
+  app.UseExceptionHandler("/Home/Error");
+  app.UseHsts();
+  app.UseHttpsRedirection();
+}
+
+SeedDatabase();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSession();
+app.UseCookiePolicy();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+var localizationOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>().Value;
+app.UseRequestLocalization(localizationOptions);
+
+app.UseEndpoints(endpoints =>
+{
+  endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+  endpoints.MapRazorPages();
+});
+
+app.Run();
+
+void SeedDatabase()
+{
+  using var scope = app.Services.CreateScope();
+  var dbSeeder = scope.ServiceProvider.GetRequiredService<ISeederToDatabase>();
+  dbSeeder.HighlightDatabaseAsync();
 }
