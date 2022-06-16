c2cac42444f5d08889f6af8c5faecec2a9d429cb
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2022-06-16 18:26:53 +0200

    improve the Program.cs config

--- EngineerApplication/Program.cs
+++ EngineerApplication/Program.cs
@@ -146,4 +146,10 @@ void SeedDatabase()
   dbSeeder.HighlightDatabaseAsync();
 }
 
+#pragma warning disable S1118 // Utility classes should not have public constructors
+#pragma warning disable S3903 // Types should be defined in named namespaces
+#pragma warning disable CA1050 // Zadeklaruj typy w przestrzeniach nazw
 public partial class Program { }
+#pragma warning restore CA1050 // Zadeklaruj typy w przestrzeniach nazw
+#pragma warning restore S3903 // Types should be defined in named namespaces
+#pragma warning restore S1118 // Utility classes should not have public constructors
